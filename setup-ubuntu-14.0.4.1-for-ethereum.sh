df275cf699ba5d161b398e4c37c03ce6640e8a2e
SimonJester <joelc.usa@gmail.com>
2015-02-10 23:19:22 -0700

    added shit

--- setup-ubuntu-14.0.4.1-for-ethereum.sh
+++ setup-ubuntu-14.0.4.1-for-ethereum.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
+sudo apt-get update -y && sudo apt-get dist-upgrade && sudo apt-get autoremove
+sudo apt-get install -y python-setuptools python-dev build-essential cmake
+easy_install pip
+pip install --upgrade virtualenv
+pip install -U pytest pytest-cov
+sudo apt-get install -y curl && vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen/vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle && git clone git://github.com/klen/python-mode.git
+cd ~/.vim/bundle/python-mode
+cd ~/.vim/bundle && git clone https://github.com/scrooloose/syntastic.git
+cd ~/.vim/bundle && git clone https://github.com/Valloric/YouCompleteMe.git
+cd ~/.vim/bundle/YouCompleteMe && git submodule update --init --recursive && ./install.sh --clang-completer
 
